c4209039b9380135a946c238647c2729d308ef80
amber-85 <amber.an.bo@gmail.com>
2024-10-14 23:46:55 +0200

    complete 50% of conditionals bootcamp until BMI

---
+++ index.js
@@ -0,0 +1,89 @@
+// 1 assign: bigger than //
+
+// let a=10;
+// let b=20;
+// if (a>b){
+//     console.log("a är större än b")
+// }
+
+// else{
+//     console.log("a är mindre än b")
+// }
+
+
+
+
+
+// //2: equals or not//
+// let c='apple';
+// let d='pear';
+// if (c===d){
+//     console.log("c är lika d")
+// }
+
+// else{
+//     console.log(c + " "+"är inte"+" "+d)
+// }
+
+
+
+
+
+//3: biggest number within two numbers. I don't know how to select two numbers//
+// Biggest Gör ett program som tar emot två stycken numeriska värden. Programmet ska utvärdera vilket tal som är störst och skriva ut det..//
+
+
+
+
+
+//4: even or not//
+// const userInput=Number(prompt('skriv in din ålder?'))
+// if (userInput %2===0){
+//     console.log("this number is even");
+// }
+// else{
+//     console.log("this number is odd");
+// }
+  
+
+
+
+
+//5. lisebergvakten med Helix//
+// let userHeight={};
+// const userInputHeight=Number(prompt('sensor detected height'));
+// // userHeight=userInputHeight;
+// console.log(Boolean(userInputHeight>150))
+
+
+// const userInputHeight=Number(prompt("sensor detected height"))
+// function greaterThanStandardHeight(userInputHeight){
+//     if (userInputHeight>150){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// console.log(greaterThanStandardHeight(userInputHeight))
+
+
+
+
+
+//6.lisebergsvakten 2//
+
+// let userInputDayPass=prompt("enter your daypass-small, medium, large and platinum")
+// function findCorrectDayPass(userInputDayPass){
+//     if(userInputDayPass==="large" || userInputDayPass==="platinum"){
+//         console.log("Balder is right on the way")
+//     }
+//     else{
+//         console.log("Balder is not ready for you yet")
+//     }
+
+// }
+// console.log(findCorrectDayPass(userInputDayPass))
+
+
+
